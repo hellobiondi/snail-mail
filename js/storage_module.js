098d4794acc1145246c2d47a06c01d454d281d45
hrtan1125 <60460382+hrtan1125@users.noreply.github.com>
2021-11-12 22:16:52 +0800

    Update storage_module.js

--- js/storage_module.js
+++ js/storage_module.js
@@ -1,4 +1,4 @@
-import { getStorage, getDownloadURL, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-storage.js";
+import { getStorage, getDownloadURL, ref, uploadBytes, uploadString } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-storage.js";
 
 const storage = getStorage();
 
@@ -13,6 +13,39 @@ export function getImages(img){
     })
   });
 }
+
+export function UploadResizeImages(uid,item){
+  var reader = new FileReader();
+
+  //image turned to base64-encoded Data URI.
+  reader.readAsDataURL(item);
+  reader.name = item.name;//get the image's name
+  reader.size = item.size; //get the image's size
+  reader.onload = function(event) {
+    var img = new Image();//create a image
+    img.src = event.target.result;//result is base64-encoded Data URI
+    img.name = event.target.name;//set name (optional)
+    img.size = event.target.size;//set size (optional)
+    img.onload = function(el) {
+      var elem = document.createElement('canvas');//create a canvas
+
+      //scale the image to 600 (width) and keep aspect ratio
+      //var scaleFactor = resize_width / el.target.width;
+      elem.width = 200;
+      elem.height = 200;
+
+      //draw in canvas
+      var ctx = elem.getContext('2d');
+      ctx.drawImage(el.target, 0, 0, elem.width, elem.height);
+
+      //get the base64-encoded Data URI from the resize image
+      var srcEncoded = ctx.canvas.toDataURL('image/png', 1);
+  const storageRef = ref(storage, uid+"/profilepic.jpeg");
+  uploadString(storageRef, srcEncoded, 'data_url').then((snapshot) => {
+    console.log('Successfully uploaded!');
+    resolve(uid+"/profilepic");
+  });
+}}}
 
 export function uploadImages(uid,file){
   const storageRef = ref(storage, uid+"/profilepic.jpeg");
